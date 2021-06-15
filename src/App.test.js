2f33b9ed949d7392d05c5e88337e1fcd6f0848f4
nuvenn <ssales.daniel@gmail.com>
2021-06-15 17:50:14 -0300

    app tests added

--- src/App.test.js
+++ src/App.test.js
@@ -1,1 +1,14 @@
-test("renders learn react link", () => {});
+import { shallow } from "enzyme";
+
+import App from "./App";
+import { findByTestAttr } from "../test/testUtils";
+
+const setup = () => {
+  return shallow(<App />);
+};
+
+it("should renders without error", () => {
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, "component-app");
+  expect(appComponent.length).toBe(1);
+});
