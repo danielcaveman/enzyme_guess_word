04913175a6993628cdccc74cd27ffa6b50ebcef3
Daniel Sales <daniel.sales@petco.com>
2021-07-30 14:41:50 -0300

    add guessWord reducer

--- src/reducers/guessedWordsReducer.js
+++ src/reducers/guessedWordsReducer.js
@@ -1,5 +1,12 @@
-const guessedWordsReducer = (state, action) => {
-  return null
+import { actionTypes } from "../actions";
+
+const guessedWordsReducer = (state=[], action) => {
+  switch(action.type) {
+    case actionTypes.GUESS_WORD:
+      return [...state, action.payload];
+    default:
+      return state;  
+  };
 };
 
 export default guessedWordsReducer;
